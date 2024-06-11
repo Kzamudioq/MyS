ce271e10246a3a5e7076f8ca10c95a4d3057ffb8
Kzamudioq <kzamudioquintero@gmail.com>
2024-06-11 12:27:56 -0500

    Proyecto final

---
+++ Trabajo final/2. Codigo Verilog/IP_CORE.sdk/Cordic_rotador/Debug/src/Cordic_rotador.d
@@ -0,0 +1,40 @@
+src/Cordic_rotador.o src/Cordic_rotador.o: ../src/Cordic_rotador.c \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/cordic_ip.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_assert.h
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/cordic_ip.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../Cordic_rotador_bsp/ps7_cortexa9_0/include/xil_assert.h:
